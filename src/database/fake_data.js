a15687a29835e39459c2e405f6e71670c3c3f429
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-08-09 21:36:12 -0300

    aula 5

---
+++ src/database/fake_data.js
@@ -0,0 +1,24 @@
+const proffys = [
+	{
+		name:"Diego Fernandes",
+		avatar:"https://avatars.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+		whatsapp:"31987541238",
+		bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratórios e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+		subject:"Química",
+		cost:"20",
+		weekday:[0] ,
+		time_from:[720] ,
+		time_to:[1220]
+	},
+	{
+		name:"Pudim",
+		avatar:"http://pudim.com.br/pudim.jpg",
+		whatsapp:"1298760001",
+		bio:"Os pudins antigos são apresentados em forma de massa sólida, constituída pela mistura de vários ingredientes unidos por uma liga que pode incluir farinha de trigo (como no Pudim de Yorkshire), sangue (pudim preto), ovos (pudim de pão), ou uma mistura de gordura e farinha ou algum outro cereal (pudim de ameixa). Esses tipos de pudim podem ser assados, cozidos ou fervidos. Esse tipo ainda é comum em vários lugares, especialmente na Grã-Bretanha, e pode ser o prato principal de uma refeição ou a sobremesa.<br>Como prato principal, pudins cozidos eram uma refeição comum a bordo dos navios da Marinha Real nos séculos XVIII e XIX, e constituíam-se no modo usual pelo qual as rações diárias de farinha e gordura eram preparadas.<br>Muitos pudins de sobremesa desse tipo lembram bolos, mas são mais úmidos e geralmente servidos em bocados numa tigela, em vez de fatias, acompanhado de manjar ou sorvete. Esse é o tipo de prato que a palavra pudim evoca à Austrália (por exemplo, o pudim de Natal ou a forma britânica do pudim de chocolate). Embora outros pratos sejam denominados pudins na Austrália (por exemplo, o pudim de arroz ou o pudim instantâneo, os quais pertencem à segunda categoria, abaixo), em tais casos a palavra é quase sempre qualificada com um adjetivo.",
+		subject:"Física",
+		cost:"3.99",
+		weekday:[0],
+		time_from:[720] ,
+		time_to:[1220]
+	}
+]
--- src/server.js
+++ src/server.js
@@ -1,105 +1,15 @@
-const proffys = [
-	{
-		name: "Diego Fernandes", 
-		avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-		whatsapp: "12312321", 
-		bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-		subject: "Química", 
-		cost: "20,00", 
-		weekday: [0],
-		timeFrom: [123],
-		timeTo: [1220]
-	},
-	{
-		name: "Daniele Evangelista", 
-		avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-		whatsapp: "12312321", 
-		bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-		subject: "Química", 
-		cost: "20,00", 
-		weekday: [1],
-		timeFrom: [123],
-		timeTo: [1220]
-	},
-	{
-		name: "Mayk Brito", 
-		avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
-		whatsapp: "12312321", 
-		bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-		subject: "História", 
-		cost: "20,00", 
-		weekday: [1],
-		timeFrom: [123],
-		timeTo: [1220]
-	}
-]
-
-const subjects = ["Artes", 
-		  "Biologia", 
-		  "Ciências", 
-		  "Educação Física", 
-		  "Física", 
-		  "Geografia", 
-		  "História", 
-		  "Matemática", 
-		  "Português", 
-		  "Química",
-		 ]
-
-const weekdays = ["Domingo", 
-		  "Segunda-feira", 
-		  "Terça-feira", 
-		  "Quarta-feira", 
-		  "Quinta-feira", 
-		  "Sexta-Feira", 
-		  "Sábado", 
-		 ]
-
-function getSubject(subjectNumber) {
-	const arrayPosition = +subjectNumber - 1
-	return subjects[arrayPosition]
-}
-
-function pageLanding(req, res) {
-	return res.render("index.html")	
-}
-
-function pageStudy(req, res) {
-	const filters = req.query
-
-	/*console.log(filters) */
-
-	return res.render("study.html", { proffys, filters, subjects, weekdays })	
-}
-
-function pageGiveClasses(req, res) {
-	const data = req.query
-	const isEmpty = Object.keys(data).length == 0
-	if(!isEmpty) {
-		data.subject = getSubject(data.subject)
-		proffys.push(data)
-		return res.redirect("/study")
-	}
-
-	/*console.log(dados) */
-
-	return res.render("give-classes.html" , { subjects, weekdays })	
-}
-
 const express = require('express')
 const server = express()
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
 
 const nunjucks = require('nunjucks')
-nunjucks.configure('src/views', {
-	express: server,
-	noCache: true,
-})
+nunjucks.configure('src/views', { express: server, noCache: true, })
 
-server.use(express.static("public"))
-
+server
+.use(express.urlencoded({extended: true}))
+.use(express.static("public"))
 .get("/", pageLanding)
 .get("/study", pageStudy)
-.get("/give-classes", pageGiveClasses) 
-
+.get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 .listen(5500)
-
